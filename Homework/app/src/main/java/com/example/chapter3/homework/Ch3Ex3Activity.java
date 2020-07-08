e61fc98a16ccc90e6ab5b021b40f184b24b51198
SikieikiYamada <1852532209@qq.com>
2020-07-08 23:52:22 +0800

    ver1

--- Homework/app/src/main/java/com/example/chapter3/homework/Ch3Ex3Activity.java
+++ Homework/app/src/main/java/com/example/chapter3/homework/Ch3Ex3Activity.java
@@ -1,7 +1,19 @@
 package com.example.chapter3.homework;
 
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TableLayout;
+
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
 
 /**
  * 使用 ViewPager 和 Fragment 做一个简单版的好友列表界面
@@ -10,6 +22,8 @@ import android.os.Bundle;
  * 3. 对于好友列表 Fragment，使用 Lottie 实现 Loading 效果，在 5s 后展示实际的列表，要求这里的动效是淡入淡出
  */
 public class Ch3Ex3Activity extends AppCompatActivity {
+    PlaceholderFragment placeholderFragment;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,9 +33,30 @@ public class Ch3Ex3Activity extends AppCompatActivity {
 
 
         // TODO: ex3-1. 添加 ViewPager 和 Fragment 做可滑动界面
-
+        ViewPager viewPager = findViewById(R.id.view_pager);
 
 
         // TODO: ex3-2, 添加 TabLayout 支持 Tab
+        TabLayout tabLayout = findViewById(R.id.tab_layout);
+        viewPager.setAdapter(new FragmentPagerAdapter(getSupportFragmentManager()) {
+            @Override
+            public Fragment getItem(int i) {
+                return new PlaceholderFragment();
+            }
+
+            @Override
+            public int getCount() {
+                return 2;
+            }
+
+            @Nullable
+            @Override
+            public CharSequence getPageTitle(int position) {
+                if(position==0) return "好友列表";
+                else if(position==1) return "我的好友";
+                else return "";
+            }
+        });
+        tabLayout.setupWithViewPager(viewPager);
     }
 }
--- Homework/app/src/main/java/com/example/chapter3/homework/PlaceholderFragment.java
+++ Homework/app/src/main/java/com/example/chapter3/homework/PlaceholderFragment.java
@@ -4,23 +4,36 @@ package com.example.chapter3.homework;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.airbnb.lottie.LottieAnimationView;
+
+import javax.sql.StatementEvent;
 
 public class PlaceholderFragment extends Fragment {
+    private String[] data = {"aa", "bb", "cc", "dd", "aa", "bb", "cc", "dd", "aa", "bb", "cc", "dd", "aa", "bb", "cc", "dd"};
+    private static final String TAG = "PlaceholderFragment";
 
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // TODO ex3-3: 修改 fragment_placeholder，添加 loading 控件和列表视图控件
+        Log.d(TAG, "onCreateView");
         return inflater.inflate(R.layout.fragment_placeholder, container, false);
     }
 
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
+        Log.d(TAG, "onActiveCreated");
+        final LottieAnimationView animationView = (LottieAnimationView) getActivity().findViewById(R.id.animation_view);
+        final ListView listView = (ListView) getActivity().findViewById(R.id.list_view);
 
         getView().postDelayed(new Runnable() {
             @Override
